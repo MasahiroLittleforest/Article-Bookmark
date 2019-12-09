4b87aa73bbe12415e72923c9b35005a27c5c6bf4
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-12-10 02:08:20 +0900

    add website with Firestore

--- public/main.js
+++ public/main.js
@@ -1,6 +1,4 @@
-var numOfLinks;
 var urls = [];
-
 
 function getUrls() {
   urls = document.getElementsByClassName('url_link');
@@ -18,3 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
   var elem = document.querySelectorAll('.modal');
   var instance = M.Modal.init(elem);
 });
+
+
+const db = firebase.firestore();
+
+function addWebsite(_name, _url) {
+  db.collection("websites").add({
+    name: _name,
+    url: _url
+  })
+    .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+    });
+}
+
+document.getElementById('add').onclick = function () {
+  var websiteName = document.forms.add_website.website_name.value;
+  var websiteUrl = document.forms.add_website.website_url.value;
+  addWebsite(websiteName, websiteUrl);
+};
